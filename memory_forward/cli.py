00fcb302ea459aa8ab1a5fce0c6b7b2a06d1f9f9
Justin Robertson <justin.keith.robertson@gmail.com>
2022-05-15 17:08:16 -0500

    Working on actual commands

--- memory_forward/cli.py
+++ memory_forward/cli.py
@@ -1,6 +1,6 @@
 from typer import Typer
-from memory_forward.app import hello
-from memory_forward.app import goodbye
+from memory_forward.commands import hello
+from memory_forward.commands import goodbye
 
 app = Typer()
 app.command()(hello.hello)
